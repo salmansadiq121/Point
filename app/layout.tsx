f31de84db618f20471609382088479cac61cef80
Salman Sadiq <salmansadiq1015@gmail.com>
2025-10-07 13:47:13 +0500

    latest udates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,30 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { GeistSans } from "geist/font/sans"
-import { GeistMono } from "geist/font/mono"
-import { Analytics } from "@vercel/analytics/next"
-import { Suspense } from "react"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Analytics } from "@vercel/analytics/next";
+import { Suspense } from "react";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
-}
+  title: "Point",
+  description: "Point is a service provider for all your essentials.",
+  generator: "Next.js",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable} antialiased scroll-smooth`}>
+      <body
+        className={`font-sans ${GeistSans.variable} ${GeistMono.variable} antialiased scroll-smooth`}
+      >
         <Suspense fallback={null}>{children}</Suspense>
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
